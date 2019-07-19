49a15b668e1ca20b29340d566e7cbb297c73da51
tienne <wvetienne@gmail.com>
2019-07-19 14:58:19 +0200

    selection filtre, resultat graph onclick begin date selection,

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var FilterDataSelected={};
+var FilterSelected="";
+var ListNames=[];
 function newActivity(name){
   const activity = {
     name:"",
@@ -12,7 +15,7 @@ function listDateSelected( datajson){
   var dates=[];
   datajson.data.forEach(function(element){
     if (!dates.includes(element.date)){
-      dates.push(element.date);
+      dates.push((element.date));
     }
   });
   return dates;
@@ -58,7 +61,7 @@ function newFilter( datajson,dates,name){
       e4.countTmp=0;
     });
   });
-    console.log(filter);
+    //console.log(filter);
   return filter;
 }
 function dataToConf(filter){
@@ -118,7 +121,17 @@ function setGraph(filter,dates){
           enabled: true,
           color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
         }
-      }
+      },
+      series: {
+            cursor: 'pointer',
+            point: {
+                events: {
+                    click: function(){
+                      resultOnClickGraph(this.category,this.y,this.stackTotal,filter.name);
+                    }
+                }
+            }
+        }
     },
     series: Conf
   });
@@ -131,30 +144,78 @@ function selectActivity(filter,dates,name){
   var listIndex=[]
   findActiviy.listdata.forEach(function(element){
     if (element == 0){
-      dates.splice(index,1);
       listIndex.unshift(index);
     }
   index++;
   });
   listIndex.forEach(function(element){
     findActiviy.listdata.splice(element,1);
+    dates.splice(element,1);
+
   });
   filter.listActivity = filter.listActivity.filter(function(element){
     return element == findActiviy
   });
 }
-function onLoadData(link,filtername,activityName){
+function createText(text){
+  var element = document.createElement("p");
+  var node = document.createTextNode(text);
+  element.appendChild(node);
+  return element;
+}
+function resultOnClickGraph(date,nbTask,totalTask,filterName){
+  var currentDiv = document.getElementById("graphInfo");
+  while (currentDiv.firstChild) {
+      currentDiv.removeChild(currentDiv.firstChild);
+  }
+  //divText = document.createElement("p");
+  //var nextLine=  document.createElement("br");
+  //var text = document.createTextNode(name);
+  currentDiv.appendChild(createText(("Date : "+date)));
+  currentDiv.appendChild(createText((filterName+" : "+nbTask)));
+  currentDiv.appendChild(createText(("Total of "+filterName+" : "+date)));
+}
+function selectFilter(dataset,filterName){
+  return dataset.studyRisk.find( filter => filter.name==filterName);
+}
+function editActivityFunction (filterName){
+  var currentDiv = document.getElementById("activities");
+  while (currentDiv.firstChild) {
+      currentDiv.removeChild(currentDiv.firstChild);
+  }
+  listbutton(ListNames,currentDiv,FilterDataSelected.name);
+  currentDiv.childNodes.forEach(function (activity){
+    activity.onclick=function(){
+      onLoadData('data.json',filterName,activity.id,)
+    }
+  });
+}
+
+function onLoadData(link,filtername,activityName,callback){
   var req = new XMLHttpRequest();
   req.overrideMimeType("application/json");
   req.open('GET', link, true);
   req.onload  = function() {
      var dataset = JSON.parse(req.responseText);
-     var DatesSelected = listDateSelected(dataset);
-     const Filter = newFilter(dataset,DatesSelected,filtername);
-     if(activityName!=false){
+     FilterDataSelected=selectFilter(dataset,filtername)
+     var DatesSelected = listDateSelected(FilterDataSelected);
+     var date = stringToDate(DatesSelected[0]);
+     dateToString(date);
+     ListNames = listNameactivities(FilterDataSelected,DatesSelected);
+     const Filter = newFilter(FilterDataSelected,DatesSelected,filtername);
+     if(activityName!="false"){
        selectActivity(Filter,DatesSelected,activityName);
      }
      setGraph(Filter,DatesSelected);
+     if (callback) callback();
   };
   req.send(null);
+}
+
+function stringToDate(value){
+  var dateList = value.split("-");
+  return new Date(dateList[2],dateList[1],dateList[0]);
+}
+function dateToString(date){
+  return date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getYear();
 }
--- script2.js
+++ script2.js
@@ -3,33 +3,61 @@ document.body.onload = initLoad("data.json",);
 
 function newHtmlActivity(name,filterName){
   var div = document.createElement('button');
+  div.setAttribute("class","btn btn-light")
   div.setAttribute("id", name);
   div.onclick=function(){
-    onLoadData('data.json',filterName,name);
+    onLoadData('data.json',filterName,name,);
   };
-  div.setAttribute("style", "color:red;width:100%;height:25px;");
+  div.setAttribute("style", "width:100%;height:5vh;");
   var text = document.createTextNode(name);
   div.appendChild(text);
   return div
 }
-function listbutton(listNameActivities,div){
-  var allActivities = newHtmlActivity("all Activities","Task failed");
+function listbutton(listNameActivities,div,filterName){
+  var allActivities = document.createElement('button');
   allActivities.setAttribute("id", "false");
-  div.appendChild(newHtmlActivity(false));
+  allActivities.onclick=function(){
+    onLoadData('data.json',filterName,"false",);
+  };
+  allActivities.setAttribute("class","btn btn-light")
+  allActivities.setAttribute("style", "width:100%;height:5vh;");
+  var activityText = document.createTextNode("all Activities");
+  allActivities.appendChild(activityText);
+  div.appendChild(allActivities);
   listNameActivities.forEach(function(activity){
-    div.appendChild(newHtmlActivity(activity));
+    div.appendChild(newHtmlActivity(activity,filterName));
   });
 }
+function initFormFilter(dataset){
+  var currentDiv = document.getElementById("filterForm");
+  dataset.studyRisk.forEach(function (filter) {
+    var optionDiv = document.createElement('option');
+    optionDiv.setAttribute("id",filter.name);
+    var optionText = document.createTextNode(filter.name);
+    optionDiv.appendChild(optionText);
+    currentDiv.appendChild(optionDiv);
+  });
+  currentDiv.onchange=function(){
+    onLoadData('data.json',document.getElementById("filterForm").value,"false",function(){
+      editActivityFunction(document.getElementById("filterForm").value);
+    });
+  };
+}
+
 function initLoad(link){
   var req = new XMLHttpRequest();
   req.overrideMimeType("application/json");
   req.open('GET', link, true);
   req.onload  = function() {
      var dataset = JSON.parse(req.responseText);
-     var datesSelected = listDateSelected(dataset);
-     var listNames = listNameactivities(dataset,datesSelected);
+     initFormFilter(dataset);
+     var firstFilter = document.getElementById("filterForm");
+     FilterDataSelected = selectFilter(dataset,(document.getElementById("filterForm")).firstChild.nextSibling.id);
+     var datesSelected = listDateSelected(FilterDataSelected);
+     ListNames = listNameactivities(FilterDataSelected,datesSelected);
      var currentDiv = document.getElementById("activities");
-     listbutton(listNames,currentDiv);
+     listbutton(ListNames,currentDiv,FilterDataSelected.name);
+     onLoadData('data.json',FilterDataSelected.name,"false",);
   };
   req.send(null);
 }
